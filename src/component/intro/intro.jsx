fa9a4b2469d789dacf7c9916fe9b9fda5f849e11
Ola Adeeko <ojadeeko@gmail.com>
2022-10-12 11:35:57 +0100

    responsiveness

--- src/component/intro/intro.jsx
+++ src/component/intro/intro.jsx
@@ -1,17 +1,8 @@
 import React from "react";
-
 import "./intro.css";
-// import Vector from "../../assets/stripe.png";
-// import Morph from "../../assets/morph.png";
 import Guy from "../../assets/guy2.png";
-// import Spart1 from "../../assets/spark-left.png";
-// import Spart2 from "../../assets/spark-right.png";
-// import HeadShot from "../../assets/headshot.jpg";
 import CustomButton from "../button/button";
 import { useNavigate } from "react-router";
-// import FloatingDiv from "../floating/floating";
-// import { motion } from "framer-motion";
-// import Logo from "../../assets/logo1.png";
 
 const Intro = () => {
   const navigate = useNavigate();
@@ -22,13 +13,10 @@ const Intro = () => {
         <div className="intro-box">
           <div className="intro-content">
             <span>value proposition</span>
-            <span>
-              {/* Get your job done by <br className="break" /> working students.{" "} */}
-              Giving undergraduates access to a seamless world
-            </span>
+            <span>Giving undergraduates access to a seamless world</span>
             <span>
               We create a platform for connecting African talents to a world of
-              opportunities.Pioneering the new undergraduate experience in
+              opportunities. Pioneering the new undergraduate experience in
               Africa.
             </span>
 
